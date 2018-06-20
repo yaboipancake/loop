b5bed6c63601325c832155f0d0de2a238cdf26cd
yaboipancake <40213312+yaboipancake@users.noreply.github.com>
2018-06-20 13:45:37 -0400

    Add files via upload

---
+++ second.js
@@ -0,0 +1,32 @@
+console.log("Display the numbers from 1 to 20.");
+for(var i = 1; i <= 20; i++) {
+   console.log(i);
+}
+console.log("Display the numbers from 2 to 20.");
+for(var i = 2; i <= 20; i+=2) {
+   console.log(i);
+}
+console.log("Display the numbers from 1 to 20.");
+for(var i = 1; i <= 20; i+=2) {
+   console.log(i);
+}
+console.log("Display the numbers from 5 to 100.");
+for(var i = 5; i <= 100; i+=5) {
+   console.log(i);
+}
+console.log("Display the numbers from 1 to 20.");
+for(var i = 1; i <= 10; i++) {
+   console.log(i*i);
+}
+console.log("Display the numbers from 1 to 20.");
+for(var i = 1; i <= 20; i+=2) {
+   console.log(i);
+}
+console.log("Display the numbers from 1 to 20.");
+   for(var i = 20; i >= 1; i-=1) {
+      console.log(i);
+    }
+    console.log("Display the numbers from 2 to 20.");
+for(var i = 20; i >= 1; i-=2) {
+   console.log(i);
+}
